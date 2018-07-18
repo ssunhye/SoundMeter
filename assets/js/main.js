3c7cade7a4ae9ed96538f89203ef381e1ccd11e1
ssunhye <33833780+ssunhye@users.noreply.github.com>
2018-07-18 18:41:33 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,44 +65,44 @@ function gotStream(stream) {
         // data=meter.volume;
 
         if(data<10){
-            desc="  숨쉬는 소리";
+            desc="숨쉬는 소리";
         }
         else if(data<20){
-            desc="  나뭇잎 스치는 소리";
+            desc="나뭇잎 스치는 소리";
         }
         else if(data<30){
-            desc="  속삭이는 소리";
+            desc="속삭이는 소리";
         }
         else if(data<40){
-            desc="  조용한 도서관";
+            desc="조용한 도서관";
         }
         else if(data<50){
-            desc="  조용한 사무실";
+            desc="조용한 사무실";
         }
         else if(data<60){
-            desc="  보통 대화소리";
+            desc="보통 대화소리";
         }
         else if(data<70){
-            desc="  진공청소기 소리";
+            desc="진공청소기 소리";
         }
         else if(data<80){
-            desc="  시끄러운 음악";
+            desc="시끄러운 음악";
         }
         else if(data<90){
-            desc="  잔디깎기 소리";
+            desc="잔디깎기 소리";
         }
         else if(data<100){
-            desc="  모터사이크 소리";
+            desc="모터사이크 소리";
         }
         else if(data<110){
-            desc="  콘서트";
+            desc="콘서트";
         }
         else{
-            desc="  고통을 주는 소음, 천둥";
+            desc="고통을 주는 소음, 천둥";
         }
 
         document.getElementById("desc").innerHTML = desc;
-        document.getElementById("show").innerHTML = data;
+        document.getElementById("show").innerHTML = data+"dB";
 
         Plotly.extendTraces('chart',{y:[[data]]}, [0]);
         ++cnt;
